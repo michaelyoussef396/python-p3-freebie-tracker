cfc4d510291fb2d4d49f049adf424e5a639cc2b7
michael <michaelyoussef396@gmail.com>
2024-05-13 20:03:02 +1000

    added freebie model

--- lib/models.py
+++ lib/models.py
@@ -9,6 +9,7 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -19,11 +20,24 @@ class Company(Base):
     def __repr__(self):
         return f'<Company {self.name}>'
 
+
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    dev = relationship("Dev", backref="freebies")
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    company = relationship("Company", backref="freebies")
